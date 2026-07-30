4f484e3453edcc8c95b856888d15550d80ff667a
Nidhi Thakur <138863268+nidhithakur0708@users.noreply.github.com>
2026-07-30 12:19:04 +0530

    Create Number Hashing Using Map.cpp

---
+++ Number Hashing Using Map.cpp
@@ -0,0 +1,26 @@
+//Number Hashing Using Map
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    //precomute
+    map<int,int>mpp;
+    for(int i=0;i<n;i++){
+        mpp[arr[i]]++;
+    }
+    int q;
+    cin>>q;
+    while(q--){
+        int num;
+        cin>>num;
+        //fetch
+        cout<<mpp[num]<<endl;
+    }
+    return 0;
+}
