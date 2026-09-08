1c222c3bafe8a903954680c5b9f8a8c916be797f
Nidhi Thakur <138863268+nidhithakur0708@users.noreply.github.com>
2026-09-08 15:59:56 +0530

    Fix maxProfit logic and improve user prompts
    Refactor maxProfit function to correct logic and improve readability. Update user prompts for clarity.

--- Arrays/18.Best time to buy and sell stocks.cpp
+++ Arrays/18.Best time to buy and sell stocks.cpp
@@ -1,35 +1,37 @@
 /******************************************************************************
 Best time to Buy and Sell Stocks:
+
 You start from i=1 and assign a[0]=mini
 You remove cost=arr[i]-mini
 Your profit is 0
 You take max from it max(profit,cost)
 Then you reassign mini min(mini,a[i])
-
 *******************************************************************************/
 
 #include <bits/stdc++.h>
 using namespace std;
 
 int maxProfit(int arr[],int n) {
-
 	int mini=arr[0];
 	int profit=0;
-	for(int i=0; i<n; i++) {
-		int cost=arr[i]-mini;
-		int profit=max(profit,cost);
-		mini=min(mini,a[i]);
+	int cost=0;
+	for(int i=1; i<n; i++) {
+		cost=arr[i]-mini;
+		profit=max(profit,cost);
+		mini=min(mini,arr[i]);
 	}
+	return profit;
 }
 int main()
 {
 	int n;
-	cout<<"Enter size of the error"<<endl;
+	cout<<"Enter size of the array:"<<endl;
+	cin>>n;
 	int arr[n];
 	for(int i=0; i<n; i++) {
 		cin>>arr[i];
 	}
-	cout<<maxProfit(arr,n);
+	cout<<"The max profit is: "<<maxProfit(arr,n);
 
 	return 0;
 }
